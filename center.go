b146be742d881d546f5845a55b1ce98a0511f8c4
yumao77777 <52820155+yumao77777@users.noreply.github.com>
2021-04-08 15:27:56 +0800

    Create center.go

---
+++ center.go
@@ -0,0 +1,12 @@
+package centiconv
+
+import "fmt"
+
+type inch float64
+type meter float64
+
+func IToM(in inch) meter { return meter(in * 0.3048) }
+func MToI(me meter) inch { return inch(me * 3.2808) }
+
+func (in inch) String() string { return fmt.Sprintf("%g英尺", in) }
+func (me meter) String() string { return fmt.Sprintf("%g英尺", me) }
